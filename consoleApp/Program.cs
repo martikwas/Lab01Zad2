4b8e00fdef71c07a96e1e8c5bede78bc8c79d484
s27640 <s27640@pjwstk.edu.pl>
2025-03-13 22:23:32 +0100

    Modyfikacja 1

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -4,9 +4,7 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        int a =6;
-        int b = 7;
-        Console.Write(a+b);
+        Console.Write("Hello World!");
 
     }
 }
